85162ffdd88214ff17feea96c120aeb6eef70adb
Chizo <chizoma44@gmail.com>
2023-03-13 11:05:04 +0100

    I linked the weather icon element to the open weather API, for real time data.

--- src/app.js
+++ src/app.js
@@ -28,16 +28,22 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "London";
-let apiKey = "b400ae3b711a616262d18b0ca2cbe78f";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b400ae3b711a616262d18b0ca2cbe78f&units=metric";
 
-https: axios.get(apiUrl).then(displayWeather);
+let apiKey = `b400ae3b711a616262d18b0ca2cbe78f`;
+let city = `Chicago`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
