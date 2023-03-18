6b6af67f6adeec28edca7f11cfab9285c7d235a7
Chizo <chizoma44@gmail.com>
2023-03-18 18:42:42 +0100

    API integration is not yet successful, because I need to put my card details on Openweather API website to have access to the forecast info I need in my code.😥😔sad...

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}: ${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -28,7 +29,7 @@ function displayForecast() {
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `              
+      `          
               
                 <div class="col-2">
                   <div class="weather-forecast-date">${day}</div>
@@ -49,6 +50,11 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div >`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = `bc73f3a1b088bb524455cc62ao20tb24`; //`97f8e93f00107773f88eafd933ce86b7`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}&units=metric`; //`https://api.openweathermap.org/data/3.0/onecall?lat={coordinates.lat}&lon={coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -73,6 +79,8 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -117,5 +125,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displaycelsiusTemp);
 
-search("Calabar");
-displayForecast();
+search("Gombe");
