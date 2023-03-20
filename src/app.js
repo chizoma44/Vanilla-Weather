41ecb8f2493de241aa10554961e1d50d7de1394a
Chizo <chizoma44@gmail.com>
2023-03-20 16:14:34 +0100

    I removed the console.log I had been using to debug my code.

--- src/app.js
+++ src/app.js
@@ -29,7 +29,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
